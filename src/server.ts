7e01405f227a169d1620505bf96403610fd55781
David de Boer <david@ddeboer.nl>
2020-12-14 22:08:08 +0100

    Use async server

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import fastify from 'fastify';
 
 const server = fastify();
 
-server.post('/datasets', (request, reply) => {
+server.post('/datasets', async (request, reply) => {
   reply.send({hello: 'world'});
 });
 
-server.listen(3000);
+const start = async () => {
+  try {
+    await server.listen(3000);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+}
+start();
