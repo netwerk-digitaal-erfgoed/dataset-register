d99a5709c12c24c4216b902730a557c4e3d055a4
David de Boer <david@ddeboer.nl>
2020-12-15 15:22:28 +0100

    Remove var

--- src/server.ts
+++ src/server.ts
@@ -22,8 +22,7 @@ server.post('/datasets', datasetsRequest, async (request, reply) => {
 
 server.addContentTypeParser('application/ld+json', { parseAs: 'string' }, function (req, body: string, done) {
   try {
-    var json = JSON.parse(body);
-    done(null, json);
+    done(null, JSON.parse(body));
   } catch (err) {
     err.statusCode = 400;
     done(err, undefined);
