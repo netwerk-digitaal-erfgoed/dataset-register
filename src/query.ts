f9fd22462b7d5be6e5ff0864e682f7f4f65b424d
David de Boer <david@ddeboer.nl>
2021-03-25 15:20:21 +0100

    Switch Schema.org back to HTTP (#96)
    Partly revert 1cb2f51 because https://schema.org/docs/jsonldcontext.jsonld
    now returns http://schema.org again (rather than https://schema.org as it
    did previously, for a while).
    
    Probably related to Schema.org release 12.

--- src/query.ts
+++ src/query.ts
@@ -91,7 +91,7 @@ const distributionMapping = new Map([
 export const datasetType = dcat('Dataset');
 export const selectQuery = `
   PREFIX dcat: <http://www.w3.org/ns/dcat#>
-  PREFIX schema: <https://schema.org/>
+  PREFIX schema: <http://schema.org/>
   PREFIX dct: <http://purl.org/dc/terms/>
   SELECT * WHERE {
     {
