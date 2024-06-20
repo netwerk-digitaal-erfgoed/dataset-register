d619b1a26232d6e75d8d0775c20a9bc31798e55b
David de Boer <david@ddeboer.nl>
2024-06-20 22:27:17 +0200

    refactor: Use CONSTRUCT (#935)
    * This seems to work now that we require a URI for publisher/creator.
    * Replace transformation logic with a CONSTRUCT query.
    * Simplify by replacing EventEmitter listeners with for await.

--- src/query.ts
+++ src/query.ts
@@ -1,7 +1,6 @@
 import factory from 'rdf-ext';
-import {BlankNodeScoped} from '@comunica/data-factory';
 import {convertToXsdDate} from './literal.js';
-import {BlankNode, NamedNode, Quad, Quad_Object, Term} from '@rdfjs/types';
+import {NamedNode} from '@rdfjs/types';
 
 const dataset = 'dataset';
 const identifier = 'identifier';
@@ -23,14 +22,11 @@ const version = 'version';
 const includedInDataCatalog = 'includedInDataCatalog';
 
 const creatorName = 'creator_name';
-const creatorEmail = 'creator_email';
-const creatorUrl = 'creator_url';
-const creatorSameAs = 'creator_sameAs';
+const creatorType = 'creator_type';
 
+const publisherType = 'publisher_type';
 const publisherName = 'publisher_name';
 const publisherEmail = 'publisher_email';
-const publisherUrl = 'publisher_url';
-const publisherSameAs = 'publisher_sameAs';
 
 const distributionUrl = 'distribution_url';
 const distributionMediaType = 'distribution_mediaType';
@@ -49,69 +45,60 @@ export const dct = (property: string): NamedNode =>
   factory.namedNode(`http://purl.org/dc/terms/${property}`);
 export const foaf = (property: string): NamedNode =>
   factory.namedNode(`http://xmlns.com/foaf/0.1/${property}`);
-const owl = (property: string): NamedNode =>
-  factory.namedNode(`http://www.w3.org/2002/07/owl#${property}`);
 export const rdf = (property: string): NamedNode =>
   factory.namedNode(`http://www.w3.org/1999/02/22-rdf-syntax-ns#${property}`);
 
-// https://www.w3.org/TR/vocab-dcat-2/#Class:Dataset
-const datasetMapping = new Map([
-  [identifier, dct('identifier')],
-  [name, dct('title')],
-  [alternateName, dct('alternative')],
-  [description, dct('description')],
-  [license, dct('license')],
-  [dateCreated, dct('created')],
-  [datePublished, dct('issued')],
-  [dateModified, dct('modified')],
-  [language, dct('language')],
-  [source, dct('source')],
-  [keyword, dcat('keyword')],
-  // [spatial, dct('spatial')],
-  // [temporal, dct('temporal')],
-  [mainEntityOfPage, dcat('landingPage')],
-  [version, owl('versionInfo')],
-  [includedInDataCatalog, dct('isPartOf')],
-]);
-
-export const creatorMapping = new Map([
-  [creatorName, foaf('name')],
-  [creatorEmail, foaf('mbox')],
-  [creatorUrl, foaf('workplaceHomepage')],
-  [creatorSameAs, owl('sameAs')],
-]);
-
-export const publisherMapping = new Map([
-  [publisherName, foaf('name')],
-  [publisherEmail, foaf('mbox')],
-  [publisherUrl, foaf('workplaceHomepage')],
-  [publisherSameAs, owl('sameAs')],
-]);
-
-// https://www.w3.org/TR/vocab-dcat-2/#Class:Distribution
-const distributionMapping = new Map([
-  [distributionUrl, dcat('accessURL')],
-  [distributionMediaType, dcat('mediaType')],
-  [distributionFormat, dct('format')],
-  [distributionDatePublished, dct('issued')],
-  [distributionDateModified, dct('modified')],
-  [distributionDescription, dct('description')],
-  [distributionLanguage, dct('language')],
-  [distributionLicense, dct('license')],
-  [distributionName, dct('title')],
-  [distributionSize, dcat('byteSize')],
-]);
-
 export const datasetType = dcat('Dataset');
 export const sparqlLimit = 50000;
-export const selectQuery = `
+
+export const constructQuery = `
   PREFIX dcat: <http://www.w3.org/ns/dcat#>
   PREFIX dct: <http://purl.org/dc/terms/>
   PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX schema: <https://schema.org/>
   PREFIX httpSchema: <http://schema.org/>
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-  SELECT * WHERE {
+
+  CONSTRUCT {
+    ?${dataset} a dcat:Dataset ;
+      dct:title ?${name} ;
+      dct:alternative ?${alternateName} ;
+      dct:description ?${description} ;
+      dct:identifier ?${identifier} ;
+      dct:license ?${license} ;
+      dct:created ?${dateCreated} ;
+      dct:issued ?${datePublished} ;
+      dct:modified ?${dateModified} ;
+      dct:language ?${language} ;
+      dct:source ?${source} ;
+      dcat:keyword ?${keyword} ;
+      dcat:landingPage ?${mainEntityOfPage} ;
+      owl:versionInfo ?${version} ;
+      dct:isPartOf ?${includedInDataCatalog} ;
+      dct:publisher ?${publisher} ;
+      dct:creator ?${creator} ;
+      dcat:distribution ?${distribution} .
+      
+    ?${publisher} a ?${publisherType} ;
+      foaf:name ?${publisherName} ;
+      foaf:mbox ?${publisherEmail} .
+
+    ?${creator} a ?${creatorType} ;
+      foaf:name ?${creatorName} .
+      
+    ?${distribution} a dcat:Distribution ;
+      dcat:accessURL ?${distributionUrl} ;
+      dcat:mediaType ?${distributionMediaType} ;
+      dct:format ?${distributionFormat} ;
+      dct:issued ?${distributionDatePublished} ;
+      dct:modified ?${distributionDateModified} ;
+      dct:description ?${distributionDescription} ;
+      dct:language ?${distributionLanguage} ;
+      dct:license ?${distributionLicense} ;
+      dct:title ?${distributionName} ;
+      dcat:byteSize ?${distributionSize} .
+  } WHERE {
     {
       ${schemaOrgQuery('schema')}
     } UNION {
@@ -123,12 +110,12 @@ export const selectQuery = `
         dct:publisher ?${publisher} .
         
       ?${publisher} a ?foafOrganizationOrPerson ;
-        a ?publisherType ;
+        a ?${publisherType} ;
         foaf:name ?${publisherName} .
       
       OPTIONAL {
         ?${creator} a ?foafOrganizationOrPerson ;
-          a ?creatorType ;
+          a ?${creatorType} ;
           foaf:name ?${creatorName} .
       }
         
@@ -169,95 +156,6 @@ export const selectQuery = `
     }
   } LIMIT ${sparqlLimit}`;
 
-export function bindingsToQuads(binding: Map<string, Term>): Quad[] {
-  const datasetIri = binding.get('dataset') as NamedNode;
-  const quads = [
-    factory.quad(datasetIri, rdf('type'), datasetType, datasetIri),
-    ..._bindingsToQuads(datasetIri, binding, datasetMapping, datasetIri),
-  ];
-
-  if (binding.get(publisher)) {
-    const publisherNode = binding.get(publisher) as NamedNode;
-    quads.push(
-      factory.quad(datasetIri, dct('publisher'), publisherNode, datasetIri),
-      factory.quad(
-        publisherNode,
-        rdf('type'),
-        organizationOrPersonToFoaf(
-          (binding.get('publisherType') as NamedNode) || foaf('Organization')
-        ),
-        datasetIri
-      ),
-      ..._bindingsToQuads(publisherNode, binding, publisherMapping, datasetIri)
-    );
-  }
-
-  if (binding.get(creator)) {
-    const creatorNode = binding.get(creator) as NamedNode;
-    quads.push(
-      factory.quad(datasetIri, dct('creator'), creatorNode, datasetIri),
-      factory.quad(
-        creatorNode,
-        rdf('type'),
-        organizationOrPersonToFoaf(
-          (binding.get('creatorType') as NamedNode) || foaf('Organization')
-        ),
-        datasetIri
-      ),
-      ..._bindingsToQuads(creatorNode, binding, creatorMapping, datasetIri)
-    );
-  }
-
-  if (binding.get(distribution)) {
-    const distributionBlankNode = binding.get(distribution) as BlankNodeScoped;
-    quads.push(
-      factory.quad(
-        datasetIri,
-        dcat('distribution'),
-        distributionBlankNode,
-        datasetIri
-      ),
-      factory.quad(
-        distributionBlankNode,
-        rdf('type'),
-        dcat('Distribution'),
-        datasetIri
-      ),
-      ..._bindingsToQuads(
-        distributionBlankNode,
-        binding,
-        distributionMapping,
-        datasetIri
-      )
-    );
-  }
-
-  return quads;
-}
-
-function _bindingsToQuads(
-  subject: NamedNode | BlankNode,
-  binding: Map<string, Term>,
-  mapping: Map<string, NamedNode>,
-  graphIri: NamedNode
-): Quad[] {
-  const quads: Quad[] = [];
-  mapping.forEach((predicate, variable) => {
-    if (binding.get(variable)) {
-      quads.push(
-        factory.quad(
-          subject,
-          predicate,
-          binding.get(variable) as Quad_Object,
-          graphIri
-        )
-      );
-    }
-  });
-
-  return quads;
-}
-
 function schemaOrgQuery(prefix: string): string {
   return `
     ?${dataset} a ${prefix}:Dataset ;
@@ -266,19 +164,40 @@ function schemaOrgQuery(prefix: string): string {
       
     FILTER (!isBlank(?${license}))
 
-    VALUES ?organizationOrPerson { ${prefix}:Organization ${prefix}:Person }          
     OPTIONAL { 
       ?${dataset} ${prefix}:creator ?${creator} .        
-      ?${creator} a ?organizationOrPerson ;
-        a ?creatorType ; 
+      ?${creator} a ?creatorTypeRaw ;
         ${prefix}:name ?${creatorName} .
+      BIND(
+        IF(
+          ?creatorTypeRaw = ${prefix}:Organization,
+          foaf:Organization,
+          IF(
+            ?creatorTypeRaw = ${prefix}:Person,
+            foaf:Person,
+            ""
+          )
+        )
+      AS ?${creatorType})
+      FILTER(?${creatorType} != "")
     }
       
     OPTIONAL { 
-      ?${dataset} ${prefix}:publisher ?${publisher} .        
-      ?${publisher} a ?organizationOrPerson ;
-        a ?publisherType ;
+      ?${dataset} ${prefix}:publisher ?${publisher} .
+      ?${publisher} a ?publisherTypeRaw ;
         ${prefix}:name ?${publisherName} .
+      BIND(
+        IF(
+          ?publisherTypeRaw = ${prefix}:Organization,
+          foaf:Organization,
+          IF(
+            ?publisherTypeRaw = ${prefix}:Person,
+            foaf:Person,
+            ""
+          )
+        )
+      AS ?${publisherType})
+      FILTER(?${publisherType} != "")
       OPTIONAL {
         ?${publisher} ${prefix}:contactPoint/${prefix}:email ?${publisherEmail} .  
       }
@@ -324,18 +243,4 @@ function schemaOrgQuery(prefix: string): string {
     OPTIONAL { ?${dataset} ${prefix}:includedInDataCatalog ?${includedInDataCatalog} }
     OPTIONAL { ?${dataset} ${prefix}:mainEntityOfPage ?${mainEntityOfPage} }
 `;
-}
-
-function organizationOrPersonToFoaf(type: NamedNode) {
-  switch (type.value) {
-    case 'http://schema.org/Person':
-    case 'https://schema.org/Person':
-      return foaf('Person');
-    case 'http://schema.org/Organization':
-    case 'https://schema.org/Organization':
-      return foaf('Organization');
-  }
-
-  // Already in FOAF.
-  return type;
 }
