8edd14995ce8874a2dd8e26e86c6ce2fd9e394cf
David de Boer <david@ddeboer.nl>
2025-10-03 14:52:47 +0200

    chore: create browser app (#1278)

---
+++ apps/browser/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import favicon from '$lib/assets/favicon.svg';
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link href={favicon} rel="icon" />
+</svelte:head>
+
+{@render children?.()}
